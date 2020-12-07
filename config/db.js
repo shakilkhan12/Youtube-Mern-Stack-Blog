b27c89f18aa5e6317f622959bc8896a7301ea3fe
Shakil Khan <shakilkhan621@gmail.com>
2020-12-07 11:40:36 +0500

    connec mongodb database

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+module.exports = connect = async () => {
+    try {
+        const response = await mongoose.connect(process.env.URL, {useUnifiedTopology: true,useNewUrlParser: true});
+        console.log('connection created');
+    } catch (error) {
+        console.log(error);
+    }
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const connect = require("./config/db");
 require('dotenv').config()
 const app = express();
+
+// connect mongodb database
+connect();
+
 app.get('/', (req, res) => {
     res.send('Hello mern blog');
 })
